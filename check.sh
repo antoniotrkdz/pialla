d78177b4bcc3874ccaf037c8896e08f69f002e76
antoniotrkdz <antoniotrkdz@gmail.com>
2018-01-25 02:25:52 +0000

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-x = curl https://raw.githubusercontent.com/antoniotrkdz/solve/master/level
-if [ "$x" == "pialla" ]; then
+X=$(curl https://raw.githubusercontent.com/antoniotrkdz/solve/master/level)
+if [ "$X" == "pialla" ]; then
   dd if=/dev/zero of=./zero.log count=4
   echo "x has the value 'pialla'" >> ./report.log
 fi
